ce7129b3ffb75046559aec33ce8edf34f21c96fe
rpter <roland@thompson.name>
2014-02-24 13:48:31 +0000

    Plugin in

---
+++ PluginTest1/Test1.cs
@@ -0,0 +1,34 @@
+﻿using CoreApplication.Extensibility;
+using System.ComponentModel.Composition;
+
+namespace PluginTest1
+{
+    [Export(typeof(ITextExtraction))]
+    public class Test1 : ITextExtraction
+    {
+
+
+        public string Version
+        {
+            get { return "1.0.0"; }
+        }
+
+        public string Author
+        {
+            get { return "Roland Paul Thompson"; }
+        }
+           
+        public string Description
+        {
+            get { return "A test class to demonstrate how to create plugins..."; }
+        }
+
+        public string ExtractedText(string sourceText)
+        {
+            return "Some work done here to extract some text!";
+        }
+
+
+    }
+
+}
